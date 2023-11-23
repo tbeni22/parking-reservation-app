7c795f05712bb8ebee4b3be667bf1aa6dee5516e
goma04 <godormarton04@gmail.com>
2023-11-23 17:30:15 +0100

    Created BLL project, LoginDTO, UserDto, Auth/User management interfaces

---
+++ BusinessLogic/Interfaces/IUserManagement.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IUserManagement
+    {
+        Task<UserDto> CreateUser();
+
+        //returns the updated user
+        Task<UserDto> UpdateUser(UserDto user);
+        Task DeleteUser(int id);
+        Task<UserDto> GetUser(int id);
+        Task<UserDto> GetUserByEmail(string email);
+        Task<bool> UpdatePassword(string password);
+
+    }
+}
