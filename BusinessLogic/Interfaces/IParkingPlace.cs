0a5a0100151efa85012e91155e0f5e35252e411b
goma04 <godormarton04@gmail.com>
2023-11-24 21:06:52 +0100

    Refactored IParkingPlace

---
+++ BusinessLogic/Interfaces/IParkingPlace.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic.DTOs;
+using DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IParkingPlace
+    {
+        Task<ParkingPlaceDto> NewParkingPlace(ParkingPlaceDto pp);
+        Task<ParkingPlaceDto> GetParkingPlace(int ID);
+        Task DeleteParkingPlace(int ID);
+        Task<ParkingPlaceDto> UpdateParkingPlace(ParkingPlaceDto pp);
+    }
+}
