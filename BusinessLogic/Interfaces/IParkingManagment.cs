52f99fd87376e7ae8dd150bea0b06676c6f8b35d
batka-gabor <batka.gabor@freemail.hu>
2023-11-24 13:54:24 +0100

    Interface implementations

---
+++ BusinessLogic/Interfaces/IParkingManagment.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IParkingManagment
+    {
+        Task<ParkingPlace> CreateParkingPlace(String name, bool disabled);
+
+        Task updateParkingPlaces();
+
+        Task<ParkingPlace> DeleteParkingPlace(ParkingPlace place);
+
+        Task<List<ParkingPlace>> GetParkingPlaces(int limit = int.MaxValue);
+        
+        Task<List<ParkingPlace>> GetParkingPlacesByName(String name, int limit = int.MaxValue);
+
+        Task<List<ParkingPlace>> FindByFilter(Func<ParkingPlace, bool> predicate);
+
+    }
+}
