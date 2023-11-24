520a74f5d173b57494f9fc1c4bee5c1ca59cb927
goma04 <godormarton04@gmail.com>
2023-11-24 19:07:53 +0100

    Revert "Refactored IParkingPlace"
    This reverts commit f4f76bb35c20c5314a477df7f3db394f36dc4b5d.

---
+++ DataAccess/Interfaces/IParkingPlace.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    internal interface IParkingPlace
+    {
+        void NewParkingPlace(ParkingPlace pp);
+        ParkingPlace GetParkingPlace(int ID);
+        void DeleteParkingPlace(int ID);
+        void UpdateParkingPlace(ParkingPlace pp);
+    }
+}
