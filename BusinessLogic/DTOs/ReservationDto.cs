f0315938190a55ac692318b2cc55289151854220
tbeni22 <47601385+tbeni22@users.noreply.github.com>
2023-11-24 22:05:11 +0100

    Add reservation interface

---
+++ BusinessLogic/DTOs/ReservationDto.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTOs
+{
+    public class ReservationDto
+    {
+        public int ID { get; set; }
+        [Required]
+        public DateTime Beginning { get; set; }
+        [Required]
+        public DateTime Ending { get; set; }
+        public int ParkingPlaceId { get; set; }
+        public String ParkingPlaceName { get; set; }
+        public UserDto User { get; set; }
+    }
+}
---
+++ BusinessLogic/Interfaces/IReservation.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface IReservation
+    {
+        Task<List<ReservationDto>> GetReservationsForUser(int userId);
+
+        Task<ReservationDto> GetReservation(int id);
+
+        Task<int> NewReservation(ReservationDto data);
+
+        Task<int> NewRepeatingReservation(RepeatingReservation data);
+
+        Task DeleteReservation(int reservationId);
+
+        Task<bool> NotifyAboutNoFreeSpace(ReservationDto slot);
+    }
+}
