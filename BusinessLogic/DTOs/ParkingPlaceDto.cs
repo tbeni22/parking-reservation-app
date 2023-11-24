f4f76bb35c20c5314a477df7f3db394f36dc4b5d
goma04 <godormarton04@gmail.com>
2023-11-24 18:14:56 +0100

    Refactored IParkingPlace

---
+++ BusinessLogic/DTOs/ParkingPlaceDto.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.DTOs
+{
+    public class ParkingPlaceDto
+    {
+        public int ID { get; init; }
+        public bool DisabledParking { get; set; }
+
+        public ICollection<ReservationDto> Reservations { get; set; } = new List<ReservationDto>();
+    }
+}
---
+++ BusinessLogic/Interfaces/IParkingPlace.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLogic.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+    internal interface IParkingPlace
+    {
+        Task<ParkingPlaceDto> NewParkingPlace(ParkingPlaceDto pp);
+        Task<ParkingPlaceDto> GetParkingPlace(int ID);
+        Task DeleteParkingPlace(int ID);
+        Task<ParkingPlaceDto> UpdateParkingPlace(ParkingPlaceDto pp);
+    }
+}
